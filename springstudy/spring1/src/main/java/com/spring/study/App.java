56c53fc511338dd14f8244fcb0a1592c8ce23099
lilong <lilong@hxwanda.com>
2017-12-05 10:50:42 +0800

    eureka 节点配置

--- springstudy/spring1/src/main/java/com/spring/study/App.java
+++ springstudy/spring1/src/main/java/com/spring/study/App.java
@@ -3,24 +3,74 @@ package com.spring.study;
 import com.spring.study.DefinitionAnnotation.EnableReigstryBean;
 import com.spring.study.domain.Book;
 import com.spring.study.domain.People;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.Constants;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Created by Administrator on 2017/11/27.
  */
 @SpringBootApplication
 @EnableReigstryBean(beans = {People.class})
+@EnableEurekaServer
 public class App {
-    public static void main(String args[]){
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    @Autowired
+    private Environment env;
+
+    /**
+     * Initializes order.
+     * <p>
+     * Spring profiles can be configured with a program arguments --spring.profiles.active=your-active-profile
+     * <p>
+     * You can find more information on how profiles work with JHipster on <a href="http://jhipster.github.io/profiles/">http://jhipster.github.io/profiles/</a>.
+     */
+    @PostConstruct
+    public void initApplication() {
+        log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains("dev") && activeProfiles.contains("pord")) {
+            log.error("You have misconfigured your application! It should not run " +
+                    "with both the 'dev' and 'prod' profiles at the same time.");
+        }
+        if (activeProfiles.contains("dev") && activeProfiles.contains("cloud")) {
+            log.error("You have misconfigured your application! It should not" +
+                    "run with both the 'dev' and 'cloud' profiles at the same time.");
+        }
+    }
+
+    public static void main(String args[]) throws UnknownHostException {
         SpringApplication app = new SpringApplication(App.class);
         ConfigurableApplicationContext context = app.run(args);
         ConfigurableEnvironment env = context.getEnvironment();
-        System.out.println("-----------------App is running ----------------------");
-        System.out.println("-----------------localhost: http://127.0.0.1:"+env.getProperty("server.port")+"    -----------");
+
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+
+        String configServerStatus = env.getProperty("configserver.status");
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Config Server: \t{}\n----------------------------------------------------------",
+                configServerStatus == null ? "Not found or not setup for this application" : configServerStatus);
     }
 }
